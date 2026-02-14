67bfdb9296b74944478427081629a6c8312f87a8
27SaidH <157550542+27SaidH@users.noreply.github.com>
2026-02-13 23:35:08 -0600

    Delete test/tests directory

--- test/tests/test_calculator.py
+++
@@ -1,9 +0,0 @@
-import pytest
-from src.calculator import add, divide
-
-def test_add():
-    assert add(2, 3) == 5
-
-def test_divide_by_zero_raises():
-    with pytest.raises(ValueError):
-        divide(10, 0)
